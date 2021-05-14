6a4156f0459e021fc1f650cf1880c3603b78c880
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-05-14 13:49:57 +0300

    start code

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-const express = require('express');
-const app= express();
+const express = require("express");
+const app = express();
 const port = 5000;
-app.get("/articles",(req,res)=>{
-    res.status=200;
-    console.log(res.body)
-})
-app.listen(port,()=>{
-    console.log(`the server at http://localhost:${port}`);
-})
+const articles = [
+  {
+    id: 1,
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
+    id: 2,
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
+    id: 3,
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
+app.use(express.json());
+app.get("/articles", (req, res) => {
+  res.status = 200;
+  res.json(articles);
+});
+app.listen(port, () => {
+  console.log(`the server at http://localhost:${port}`);
+});
