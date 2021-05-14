8f922faf484154cefd836bb7ed454fd314bc44bf
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-05-14 21:34:41 +0300

    I am working

--- main.js
+++ main.js
@@ -26,6 +26,45 @@ app.get("/articles", (req, res) => {
   res.status = 200;
   res.json(articles);
 });
+app.get("/articles/search", (req, res) => {
+  const user = req.query.id;
+  const found = articles.find((element) => {
+    return element.id === Number(user);
+  });
+  if (found) {
+    res.status(200);
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("User not found ");
+  }
+});
+app.get("/articles/search_1/:author", (req, res) => {
+  const user1 = req.params.author;
+  const found1 = articles.find((element) => {
+    return element.author === user1;
+  });
+  if (found1) {
+    res.status(200);
+    res.json(found1);
+  } else {
+    res.status(404);
+    res.json("User not found ");
+  }
+});
+app.post("/articles", (req, res) => {
+  // const createNewArticle = {title:req.body.title,description:req.body.description, author:req.body.author};
+  const createNewArticle = {
+    title: "Server",
+    description: "Lorem, Quam, mollitia.",
+    author: "Ayman",
+  };
+  const { title, description, author } = req.body;
+  articles.push(createNewArticle);
+  res.status(201);
+  res.json(createNewArticle);
+  //   res.json(articles);
+});
 app.listen(port, () => {
   console.log(`the server at http://localhost:${port}`);
 });
